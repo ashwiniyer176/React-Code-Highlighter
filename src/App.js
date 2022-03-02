80fb0056979d03eb32aa9720ba4c222e0c70823c
Ashwin Iyer <ashwiniyer1706@gmail.com>
2022-03-02 20:27:26 +0530

    Setup django connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [data, setData] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  async function fetchSnippets() {
+    await axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/snippets/2/",
+    })
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  React.useEffect(() => {
+    setLoading(true);
+    fetchSnippets();
+  }, []);
+  if (loading) {
+    return null;
+  } else {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>{data.code}</p>
+      </div>
+    );
+  }
 }
-
-export default App;
